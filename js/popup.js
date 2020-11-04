20951c946bb453d11aa94048da2be903763e1ae3
ddlein <70834132+ddlein@users.noreply.github.com>
2020-11-04 17:56:04 +0300

    Слайдер на JS

--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,5 @@
+// PopUp
+
 const btnContacts = document.querySelector(".button-contacts");
 const popUp = document.querySelector(".popup");
 const close = document.querySelector(".close-popup");
@@ -40,3 +42,43 @@ window.addEventListener("keydown", function (evt) {
 })
 
 
+//  Slider
+
+const firstBtn = document.querySelector(".first-btn");
+const secondBtn = document.querySelector(".second-btn");
+const thirdBtn = document.querySelector(".third-btn");
+const currentSlide = document.querySelector(".current-slide");
+const firstSlide = document.querySelector(".first-slide");
+const secondSlide = document.querySelector(".second-slide");
+const thirdSlide = document.querySelector(".third-slide");
+const currentBtn = document.querySelector(".feature-item--current");
+
+firstBtn.addEventListener("click", function(evt) {
+  evt.preventDefault;
+  secondSlide.classList.remove("current-slide");
+  thirdSlide.classList.remove("current-slide");
+  firstSlide.classList.add("current-slide");
+  secondBtn.classList.remove("feature-item--current");
+  thirdBtn.classList.remove("feature-item--current");
+  firstBtn.classList.add("feature-item--current");
+})
+
+secondBtn.addEventListener("click", function(evt) {
+  evt.preventDefault;
+  firstSlide.classList.remove("current-slide");
+  thirdSlide.classList.remove("current-slide");
+  secondSlide.classList.add("current-slide");
+  firstBtn.classList.remove("feature-item--current");
+  thirdBtn.classList.remove("feature-item--current");
+  secondBtn.classList.add("feature-item--current");
+})
+
+thirdBtn.addEventListener("click", function(evt) {
+  evt.preventDefault;
+  firstSlide.classList.remove("current-slide");
+  secondSlide.classList.remove("current-slide");
+  thirdSlide.classList.add("current-slide");
+  firstBtn.classList.remove("feature-item--current");
+  secondBtn.classList.remove("feature-item--current");
+  thirdBtn.classList.add("feature-item--current");
+})
